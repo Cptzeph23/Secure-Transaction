6d0e9b8e3620b17a8ce6a03602d620316b38a616
Cptzeph23 <asekazephaniah@gmail.com>
2025-12-12 20:22:39 +0300

    urls

--- fraudweb/fraudSystem/urls.py
+++ fraudweb/fraudSystem/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-path('transactions/', include('transactions.urls')),
+    path('transactions/', include('transactions.urls')),
 ]
 
